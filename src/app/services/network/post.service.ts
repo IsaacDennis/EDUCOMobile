c06963b846ae514bbbbae232521c0fc2ff22f703
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-10-29 20:20:15 -0300

    Creation of Post service and its methods

--- src/app/services/network/post.service.ts
+++ src/app/services/network/post.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import external from 'external.config.json';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/models/post.model';
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-
-  constructor() { }
+  baseUrl: string = external.serverUrl + '/posts';
+  constructor(private http: HttpClient) { }
+  createPost(postData: Post): Observable<Post>{
+    return this.http.post<Post>(this.baseUrl, postData);
+  }
+  deletePost(postId: number): Observable<any>{
+    return this.http.delete(this.baseUrl + `/${postId}`);
+  }
+  getPostsByGroup(groupId: number, quantity?: number, startIndex?: number): Observable<Post[]>{
+    return this.http.post<Post[]>(this.baseUrl + `/by_group/${groupId}`, {quantity, startIndex});
+  }
 }

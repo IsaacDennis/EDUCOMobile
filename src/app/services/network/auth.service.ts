77a97b13ee7581b97dc91e586aa7856464473082
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-10-30 12:42:33 -0300

    Creation of user authentication method

--- src/app/services/network/auth.service.ts
+++ src/app/services/network/auth.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import external from "external.config.json";
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  baseUrl: string = external.serverUrl + '/sessions';
+  constructor(private http: HttpClient) { }
+  authenticateUser(email: string, password: string): Observable<string>{
+    return this.http.post<string>(this.baseUrl, { email, password });
+  }
 }

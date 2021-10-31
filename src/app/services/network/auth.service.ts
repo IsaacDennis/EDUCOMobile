821e76b3e87f03b654fe8180d8124d381ff02ea6
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-10-31 00:13:04 -0300

    Authentication with token

--- src/app/services/network/auth.service.ts
+++ src/app/services/network/auth.service.ts
@@ -1,14 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+/* eslint-disable @typescript-eslint/dot-notation */
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import external from "external.config.json";
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+import external from 'external.config.json';
+import { User } from 'src/app/models/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  loggedUser: BehaviorSubject<User> = new BehaviorSubject(null);
   baseUrl: string = external.serverUrl + '/sessions';
   constructor(private http: HttpClient) { }
-  authenticateUser(email: string, password: string): Observable<string>{
-    return this.http.post<string>(this.baseUrl, { email, password });
+  authenticateUser(email: string, password: string): void{
+    this.http.post<string>(this.baseUrl, { email, password })
+      .pipe(
+        mergeMap(this.getUserFromToken)
+      )
+      .subscribe(this.loggedUser);
+  }
+  getUserFromToken(token: string): Observable<User>{
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`);
+    return this.http.get(this.baseUrl, { headers })
+      .pipe(
+        map(response => new User(
+          response['id'],
+          response['name'],
+          response['birth'],
+          response['created_at'],
+          response['updated_at'],
+          response['bio'],
+          response['avatar']
+        ))
+      );
   }
 }

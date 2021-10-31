c2a5183a3502a5144fb601d8e77a57d5bcb8a922
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-10-31 12:47:17 -0300

    Creation of UserService methods

--- src/app/services/network/user.service.ts
+++ src/app/services/network/user.service.ts
@@ -1,9 +1,31 @@
+/* eslint-disable @typescript-eslint/dot-notation */
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import external from 'external.config.json';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from 'src/app/models/user.model';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  baseUrl: string = external.serverUrl + '/users';
+  constructor(private http: HttpClient) { }
+  getUserById(userId: string): Observable<User>{
+    return this.http.get(this.baseUrl + `/by_id/${userId}`)
+      .pipe(
+        map(response => new User(
+          response['id'],
+          response['name'],
+          response['birth'],
+          response['created_at'],
+          response['updated_at'],
+          response['bio'],
+          response['avatar']
+        ))
+      );
+  }
+  registerUser(userData: User){
+    return this.http.post<string>(this.baseUrl, { ...userData });
+  }
 }

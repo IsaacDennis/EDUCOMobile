3210a6fc4f0d355f603e96fa264c8a4f63749adb
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-01 18:26:44 -0300

    HeaderComponent creation

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  @Input() searchName: string;
+  constructor() { }
+
+  ngOnInit() {}
+
+}

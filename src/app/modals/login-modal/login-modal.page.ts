bd4b24fe8a322d5c62bcac3a6a53233d5e7810dc
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-02 00:42:24 -0300

    Layout of LoginModal with dynamic text

--- src/app/modals/login-modal/login-modal.page.ts
+++ src/app/modals/login-modal/login-modal.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-modal.page.scss'],
 })
 export class LoginModalPage implements OnInit {
-
+  studyTexts = ['Matemática', 'Química', 'Física', 'Programação', 'o que quiser'];
+  currentIndex = 0;
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => {
+      const length = this.studyTexts.length;
+      if (this.currentIndex === length - 1){
+        this.currentIndex = 0;
+      } else {
+        this.currentIndex++;
+      }
+    }, 3000);
   }
 
 }

2273f828ad6b51be690df29d73dd604b9660dd84
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-02 00:42:56 -0300

    Presenting LoginModal when the application is opened

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { LoginModalPage } from './modals/login-modal/login-modal.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(private modalController: ModalController) {}
+  // TODO: salvar login após fechar o aplicativo
+  ngOnInit(){
+    this.presentLoginModal();
+  }
+  async presentLoginModal(){
+    const modal = await this.modalController.create({
+      component: LoginModalPage
+    });
+    return await modal.present();
+  }
 }

055e223f0143a7415fba4386aec753bc3ca97d63
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-02 23:21:59 -0300

    Adding login inputs and initial authentication support

--- src/app/modals/login-modal/login-modal.page.ts
+++ src/app/modals/login-modal/login-modal.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { AuthService } from 'src/app/services/network/auth.service';
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-login-modal',
@@ -8,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 export class LoginModalPage implements OnInit {
   studyTexts = ['Matemática', 'Química', 'Física', 'Programação', 'o que quiser'];
   currentIndex = 0;
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(
+    private modalController: ModalController,
+    private toastController: ToastController,
+    private loadingController: LoadingController,
+    private auth: AuthService
+  ) { }
 
   ngOnInit() {
     setInterval(() => {
@@ -19,6 +29,47 @@ export class LoginModalPage implements OnInit {
         this.currentIndex++;
       }
     }, 3000);
+    this.auth.loggedUser.subscribe(user => {
+      if (user){
+        this.loadingController.dismiss();
+        this.modalController.dismiss();
+      }
+    });
   }
+  setEmail(value: string){
+    this.email = value;
+  }
+  setPassword(value: string){
+    this.password = value;
+  }
+  startAuthentication(){
+    if (!this.email || !this.password){
+      this.presentWarning('Preencha todos os campos.');
+      return;
+    }
+    this.presentLoading();
+    this.auth.authenticateUser(this.email, this.password);
+    this.auth.loggedUser.subscribe(console.log);
 
+  }
+  async presentRegister(){
+    const modal = await this.modalController.create({
+      component: RegisterComponent
+    });
+    return await modal.present();
+  }
+  async presentWarning(message: string){
+    const warning = await this.toastController.create({
+      message,
+      color: 'danger',
+      duration: 1000
+    });
+    await warning.present();
+  }
+  async presentLoading(){
+    const loading = await this.loadingController.create({
+      message: 'Autenticando o usuário'
+    });
+    await loading.present();
+  }
 }

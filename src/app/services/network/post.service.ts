a310e5a4cd8cd795f7959d8636641b5b63c511b4
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-07 17:43:38 -0300

    Correction at getPostsByGroup typing

--- src/app/services/network/post.service.ts
+++ src/app/services/network/post.service.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/dot-notation */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import external from 'external.config.json';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Post } from 'src/app/models/post.model';
 @Injectable({
   providedIn: 'root'
@@ -16,6 +18,17 @@ export class PostService {
     return this.http.delete(this.baseUrl + `/${postId}`);
   }
   getPostsByGroup(groupId: number, quantity?: number, startIndex?: number): Observable<Post[]>{
-    return this.http.post<Post[]>(this.baseUrl + `/by_group/${groupId}`, {quantity, startIndex});
+    return this.http.post<any[]>(this.baseUrl + `/by_group/${groupId}`, {quantity, startIndex})
+      .pipe(
+        map(responses => responses.map(response => new Post(
+          response['id'],
+          response['userId'],
+          response['groupId'],
+          response['text'],
+          response['created_at'],
+          response['updated_at'],
+          response['imageId']
+        )))
+      );
   }
 }

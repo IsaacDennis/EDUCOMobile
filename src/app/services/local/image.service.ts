8269be14d26bafe31709db933cfa63c203de4203
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-14 12:00:34 -0300

    Creation of text recognition method through Tesseract

--- src/app/services/local/image.service.ts
+++ src/app/services/local/image.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import Tesseract from 'tesseract.js';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
   constructor() { }
-  async getImageFromSource(source: CameraSource){
+  async getImageFromSource(source: CameraSource): Promise<Photo>{
     return await Camera.getPhoto({
-      resultType: CameraResultType.Base64,
+      resultType: CameraResultType.Uri,
       allowEditing: true,
       quality: 100,
+      source,
     });
+  }
+  async recognizeText(photo: Photo, logger?: (worker) => void): Promise<Tesseract.RecognizeResult>{
+    return Tesseract.recognize(
+      photo.webPath,
+      'por',
+      { logger }
+    );
   }
 }

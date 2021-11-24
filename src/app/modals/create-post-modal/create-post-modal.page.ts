533dba181734f561529fa491cd4cbeaa1a80b540
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-11-24 20:06:32 -0300

    Submiting posts to the server

--- src/app/modals/create-post-modal/create-post-modal.page.ts
+++ src/app/modals/create-post-modal/create-post-modal.page.ts
@@ -4,6 +4,9 @@ import { CameraSource } from '@capacitor/camera';
 import { ActionSheetController, AlertController, LoadingController, ModalController, ToastController } from '@ionic/angular';
 import { ImageService } from 'src/app/services/local/image.service';
 import { Clipboard } from '@capacitor/clipboard';
+import { PostService } from 'src/app/services/network/post.service';
+import { AuthService } from 'src/app/services/network/auth.service';
+import { mergeMap } from 'rxjs/operators';
 @Component({
   selector: 'app-create-post-modal',
   templateUrl: './create-post-modal.page.html',
@@ -11,23 +14,63 @@ import { Clipboard } from '@capacitor/clipboard';
 })
 export class CreatePostModalPage implements OnInit {
   @Input() groupId: number;
+  postSubmitted = false;
+  text: string;
   constructor(
     private modalController: ModalController,
     private sheetController: ActionSheetController,
     private loadingController: LoadingController,
     private alertController: AlertController,
+    private toastController: ToastController,
+    private postService: PostService,
     private image: ImageService,
+    private auth: AuthService
     ) { }
 
   ngOnInit() {
   }
-  async dismissModal(){
-    await this.modalController.dismiss();
+  async submitPost(){
+    const text = this.text?.trim();
+    if (!text){
+      this.presentToast('Insira o conteúdo do post.', 'danger');
+      return;
+    }
+    await this.presentLoading('Enviando o post');
+    const nextFunction = () => {
+      this.postSubmitted = true;
+      this.presentToast('O post foi enviado.', 'success');
+      this.loadingController.dismiss();
+    };
+    const errorFunction = () => {
+      this.presentToast('Houve um erro ao enviar o post.', 'danger');
+      this.loadingController.dismiss();
+    };
+    this.auth.loggedUser
+      .pipe(
+        mergeMap(user => this.postService.createPost({
+          userId: user.id,
+          text,
+          groupId: this.groupId
+        })),
+      ).subscribe(nextFunction, errorFunction);
   }
-  async presentLoading(): Promise<Element>{
+  async dismissModal(){
+    await this.modalController.dismiss({
+      postSubmitted: this.postSubmitted
+    });
+  }
+  async presentToast(message: string, color: string){
+    const warning = await this.toastController.create({
+      message,
+      color,
+      duration: 1000
+    });
+    await warning.present();
+  }
+  async presentLoading(message: string): Promise<Element>{
     const loading = await this.loadingController.create({
       backdropDismiss: true,
-      message: '0%'
+      message
     });
     await loading.present();
     return loading.querySelector('.loading-content');
@@ -53,7 +96,7 @@ export class CreatePostModalPage implements OnInit {
           handler: async () => {
             const image = await this.image.getImageFromSource(CameraSource.Camera);
             await sheet.dismiss();
-            const loadingContent = await this.presentLoading();
+            const loadingContent = await this.presentLoading('0%');
             const logger = worker => {
               const percentage = Math.ceil(worker.progress * 100);
               loadingContent.textContent = `Analisando a imagem - ${percentage}%`;
